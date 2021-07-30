74eebb4a80b5637bfa47a1ab1f48d7282cb68b9f
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-07-30 10:40:56 +0900

    Export more functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Pai, Shupai, ShupaiType, Zupai, Fonpai, Yakuhai, isZupai, isShupai } from "./pai";
-import { parse, stringify } from "./parse";
-import { shanten } from "./shanten";
+import { parse, parseSingle, stringify } from "./parse";
+import { shanten, sameTile, shantenRecurse } from "./shanten";
 
 export {
   Pai, Shupai, ShupaiType, Zupai, Fonpai, Yakuhai, isZupai, isShupai,
-  parse, stringify,
-  shanten,
+  parse, parseSingle, stringify,
+  shanten, sameTile, shantenRecurse
 };
